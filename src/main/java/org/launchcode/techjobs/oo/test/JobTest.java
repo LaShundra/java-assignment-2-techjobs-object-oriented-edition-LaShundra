7c37fb31a4daa53f82da03be7ce59a43254d9bf8
lashundra <lashundrasdavis@att.net>
2023-05-03 22:47:26 -0500

    Completion of step 4 JobTest

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,76 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.launchcode.techjobs.oo.Job;
+import org.launchcode.techjobs.oo.Employer;
+import org.launchcode.techjobs.oo.CoreCompetency;
+import org.launchcode.techjobs.oo.PositionType;
+import org.launchcode.techjobs.oo.Location;
+
+
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+
+
+    @Test
+    public void testSettingJobId(){
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        //job1 & job2 values are not the same
+        assertFalse(job1.equals(job2));
+
+    }
+
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertTrue(testJob instanceof Job);
+
+        assertTrue(testJob.getName() == "Product tester");
+
+        assertTrue(testJob.getEmployer() instanceof Employer);
+
+        assertTrue(testJob.getEmployer().getValue() == "ACME");
+
+        assertTrue(testJob.getLocation() instanceof Location);
+
+        assertTrue(testJob.getLocation().getValue() == "Desert");
+
+        assertTrue(testJob.getPositionType() instanceof PositionType);
+
+        assertTrue(testJob.getPositionType().getValue() == "Quality control");
+
+        assertTrue(testJob.getCoreCompetency() instanceof CoreCompetency);
+
+        assertTrue(testJob.getCoreCompetency().getValue() == "Persistence");
+
+    }
+
+    @Test
+    public void testJobsForEquality(){
+        Job Job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        Job Job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+
+        assertFalse(Job1.equals(Job2));
+    }
 
 }
